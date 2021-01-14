6b6ceff409af3145bca6979af20d669439e2771a
Akash97p <akash.anjanam@gmail.com>
2021-01-14 07:57:44 +0530

    1. changed mongoDB driver . 2.added mongoDB read and write. 3.renderd database as table using ejs

---
+++ README.md
@@ -0,0 +1,8 @@
+# Iot Data logger
+<hr>
+
+<img align="right" src="https://img.shields.io/badge/DB-mongodb-informational?style=flat&logo=mongodb&logoColor=white&color=199c0e"> 
+
+<img align="right" src="https://img.shields.io/badge/Code-JavaScript-informational?style=flat&logo=javascript&logoColor=white&color=ebd234"> 
+
+<img align="right" src="https://img.shields.io/badge/Tools-node.js-informational?style=flat&logo=node.js&logoColor=white&color=5aa154">
--- app.js
+++ app.js
@@ -1,56 +1,84 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const { MongoClient } = require('mongodb');
 const path = require('path');
+const ejs = require("ejs");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({extended: true}));
-var wl = 60;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
+var wl = 60;
+var DBconn = new Boolean(false);
 //-----------------------------------------------------------------------------------------------------------------
-//mongodb URL
-const url = `mongodb+srv://kabanitech:makerspace@cluster0.f6tma.mongodb.net/<esp-data>?retryWrites=true&w=majority`
-const connectionParams = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+//mongodb
+var dbo;
+const url = `mongodb+srv://kabanitech:makerspace@cluster0.f6tma.mongodb.net/esp-data?retryWrites=true&w=majority`
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    DBconn = true;
+    dbo = db.db("esp-data");
+  });
+
+function creatCollec(name){
+    dbo.createCollection(name, function(err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+      });
 }
 
-//Connect to MongoDB atlas
-mongoose.connect(url, connectionParams)
-    .then(() => {
-        console.log('Connected to database ')
-    })
-    .catch((err) => {
-        console.error(`Error connecting to the database. \n${err}`);
-    })
-
-// Write data to DB
-
-
-// Read Data from DB
-
-
 //-----------------------------------------------------------------------------------------------------------------
 
-app.post('/', (req, res) => {
-    const body = req.body;
-    console.log(body);
-    wl = body.waterLevel;
+function getTime(input) {
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    let seconds = date_ob.getSeconds();
+    let today = year + "-" + month + "-" + date;
+    let dateTime = today + " " + hours + ":" + minutes + ":" + seconds;
+    return input == "dt" ? dateTime : today;
+}
+
+app.post('/esp', (req, res) => {
+    var newDoc = {
+        deviceID: req.body.ID,
+        dateTime: getTime("dt"),
+        waterLevel: req.body.waterLevel
+    };
+    console.log(newDoc);
+    dbo.collection(getTime("d")).insertOne(newDoc, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+      });
     res.status(201).json({});
 });
 
-app.get('/', (req, res) => {
-    console.log("get")
-    res.send("hii");
+app.get('/raw', (req, res) => {
+    dbo.collection(getTime("d")).find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.send({data : result});
+      });
+})
+
+app.get('/table', (req, res) => {
+    dbo.collection(getTime("d")).find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.render("pages/table", { data: result });
+      });
 })
 
 app.get('/python', (req, res) => {
     console.log('req received from python client');
     console.log(wl);
-    res.send({WaterLevel: wl});
+    res.send({ WaterLevel: wl });
 })
 
 app.listen(process.env.PORT || 3000, () => {
